c2a86a855870e498879e4375c45b68bcdfba6383
nazanin_ys <nazaninyousefian@yahoo.com>
2021-10-15 17:20:05 +0330

    theory left

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerTest.java
@@ -1,7 +1,137 @@
 package org.springframework.samples.petclinic.owner;
 
+import org.junit.*;
+import org.junit.experimental.theories.*;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
+import static org.mockito.Mockito.*;
 
-class OwnerTest {
+
+@RunWith(Theories.class)
+public class OwnerTest {
+	private Owner owner;
+	private Pet petMock;
+	private String petName = "pishul";
+
+	private void callAddPet() {
+		owner.addPet(petMock);
+	}
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		owner =  new Owner();
+		petMock = spy(Pet.class);
+		petMock.setName(petName);
+	}
+
+	@After
+	public void teardown() {
+		owner = null;
+		petMock = null;
+	}
+
+	@Test
+	public void testOwnerSetsWhenAddPet() {
+		callAddPet();
+		assertEquals(owner, petMock.getOwner(), "owner is not set properly");
+	}
+
+	@Test
+	public void testOwnerChangesWhenPetAddsToNewOwner() {
+		callAddPet();
+		Owner newOwner = new Owner();
+		newOwner.addPet(petMock);
+		assertEquals(newOwner, petMock.getOwner(), "owner has not changed");
+	}
+
+	@Test
+	public void testNewPetAdd() {
+		callAddPet();
+		Set<Pet> pets = owner.getPetsInternal();
+		assertTrue(pets.contains(petMock), "pet is not added");
+	}
+
+	@Test
+	public void testExistedPetNotAdd() {
+		when(petMock.isNew()).thenReturn(false);
+		callAddPet();
+		Set<Pet> pets = owner.getPetsInternal();
+		assertFalse(pets.contains(petMock), "pet with id added");
+	}
+
+	@Test
+	public void testGetExistingPet() {
+		callAddPet();
+		Pet foundPet = owner.getPet(petName);
+		assertNotNull(foundPet, "Existing pet not found");
+	}
+
+	@Test
+	public void testGetNonExistingPet() {
+		Pet foundPet = owner.getPet(petName);
+		assertNull(foundPet, "found wrong pet");
+	}
+
+	@Test
+	public void testGetPetWhenIgnoreNew() {
+		callAddPet();
+		Pet foundPet = owner.getPet(petName, true);
+		assertNull(foundPet, "returned object when not expected");
+	}
+
+
+	@Test
+	public void testGetPetsSorted() {
+		Pet pet1 = spy(Pet.class);
+		pet1.setName("Lexi");
+		Pet pet2 = spy(Pet.class);
+		pet2.setName("Luxi");
+		Pet pet3 = spy(Pet.class);
+		pet3.setName("Luxe");
+		Pet pet4 = spy(Pet.class);
+		pet4.setName("Coco");
+		owner.addPet(pet1);
+		owner.addPet(pet2);
+		owner.addPet(pet3);
+		owner.addPet(pet4);
+		List<Pet> expected = Arrays.asList(pet4, pet1, pet3, pet2);
+		List<Pet> result = owner.getPets();
+		assertEquals(expected, result);
+	}
+
+
+	@DataPoints
+	public static boolean[] ignoreNews = {true, false};
+
+	@DataPoint
+	public static String name = "pishul";
+
+	@Theory
+	public void testNoPetReturnIfNotExist(boolean ignoreNew, String name) {
+		Set<String> petNames = new HashSet<>();
+		List<Pet> pets = owner.getPets();
+		for (Pet pet :  pets)
+			petNames.add(pet.getName());
+		assumeFalse(petNames.contains(name));
+		Pet retPet = owner.getPet(name, ignoreNew);
+		assertTrue(retPet == null);
+	}
+
+	@Theory
+	public void testFindPetIfNotNew(boolean ignoreNew, String name) {
+		callAddPet();
+		Set<String> petNames = new HashSet<>();
+		List<Pet> pets = owner.getPets();
+		for (Pet pet :  pets)
+			petNames.add(pet.getName().toLowerCase());
+		assumeTrue(petNames.contains(name.toLowerCase()));
+		when(petMock.isNew()).thenReturn(false);
+		Pet retPet = owner.getPet(name, ignoreNew);
+		assertEquals(retPet.getName().toLowerCase(), name.toLowerCase());
+	}
 
 }
--- src/test/java/org/springframework/samples/petclinic/owner/PetServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetServiceTest.java
@@ -1,11 +1,62 @@
 package org.springframework.samples.petclinic.owner;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.samples.petclinic.utility.SimpleDI;
-
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.*;
+import org.slf4j.Logger;
+import org.springframework.samples.petclinic.utility.PetTimedCache;
+import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-class PetServiceTest {
+@RunWith(Parameterized.class)
+public class PetServiceTest {
+	private PetService petService;
+	private static Pet petToBeFound;
+	private Integer petId;
+	private Pet result;
+	private static Integer id1 = 610;
+	private static Integer id2 = 581;
+	@Mock
+	private PetTimedCache pets;
+	@Mock
+	private PetRepository repository;
+	@Mock
+	private OwnerRepository owners;
+	@Mock
+	private Logger criticalLogger;
 
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		petToBeFound = new Pet();
+		petToBeFound.setId(id1);
+		petService = new PetService(pets, owners, criticalLogger);
+		when(repository.findById(id2)).thenReturn(null);
+		when(repository.findById((id1))).thenReturn(petToBeFound);
+	}
+
+	@After
+	public void teardown() {
+		petService = null;
+		petToBeFound = null;
+	}
+
+	public PetServiceTest(Integer id, Pet result) {
+		this.petId = id;
+		this.result = result;
+	}
+
+	@Parameterized.Parameters
+	public static Collection<Object[]> parameters() {
+		return Arrays.asList (new Object [][] {{id1, petToBeFound}, {id2, null}});
+	}
+
+	@Test
+	public void testFindPet() {
+		assertEquals(petService.findPet(petId), result);
+	}
 
 }
