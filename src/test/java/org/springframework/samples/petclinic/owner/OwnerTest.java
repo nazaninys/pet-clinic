1f5d4d994e457d53ed8ea9a51b928d5f0c4647d3
nazanin_ys <nazaninyousefian@yahoo.com>
2021-10-15 20:16:04 +0330

    theory tests added

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerTest.java
@@ -2,11 +2,12 @@ package org.springframework.samples.petclinic.owner;
 
 import org.junit.*;
 import org.junit.experimental.theories.*;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.runner.RunWith;
 import org.mockito.*;
 import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assumptions.*;
+import static org.junit.Assume.assumeTrue;
 import static org.mockito.Mockito.*;
 
 
@@ -14,10 +15,38 @@ import static org.mockito.Mockito.*;
 public class OwnerTest {
 	private Owner owner;
 	private Pet petMock;
-	private String petName = "pishul";
+	private static String PET_NAME = "pishul";
 
 	private void callAddPet() {
 		owner.addPet(petMock);
+	}
+
+	private List<Pet> createAndAddMultiplePets() {
+		Pet pet1 = spy(Pet.class);
+		pet1.setName("Lexi");
+		Pet pet2 = spy(Pet.class);
+		pet2.setName("Luxi");
+		Pet pet3 = spy(Pet.class);
+		pet3.setName("Luxe");
+		Pet pet4 = spy(Pet.class);
+		pet4.setName("Coco");
+		owner.addPet(pet1);
+		owner.addPet(pet2);
+		owner.addPet(pet3);
+		owner.addPet(pet4);
+		return Arrays.asList(pet4, pet1, pet3, pet2);
+	}
+
+	private void addPets(List<String> petNames){
+
+		for(String name : petNames){
+			owner.addPet(new Pet() {
+				{
+					setName(name.toLowerCase());
+				}
+
+			});
+		}
 	}
 
 	@Before
@@ -25,7 +54,7 @@ public class OwnerTest {
 		MockitoAnnotations.initMocks(this);
 		owner =  new Owner();
 		petMock = spy(Pet.class);
-		petMock.setName(petName);
+		petMock.setName(PET_NAME);
 	}
 
 	@After
@@ -66,72 +95,71 @@ public class OwnerTest {
 	@Test
 	public void testGetExistingPet() {
 		callAddPet();
-		Pet foundPet = owner.getPet(petName);
+		Pet foundPet = owner.getPet(PET_NAME);
 		assertNotNull(foundPet, "Existing pet not found");
 	}
 
 	@Test
 	public void testGetNonExistingPet() {
-		Pet foundPet = owner.getPet(petName);
+		createAndAddMultiplePets();
+		Pet foundPet = owner.getPet(PET_NAME);
 		assertNull(foundPet, "found wrong pet");
 	}
 
 	@Test
 	public void testGetPetWhenIgnoreNew() {
 		callAddPet();
-		Pet foundPet = owner.getPet(petName, true);
+		Pet foundPet = owner.getPet(PET_NAME, true);
 		assertNull(foundPet, "returned object when not expected");
 	}
 
 
 	@Test
 	public void testGetPetsSorted() {
-		Pet pet1 = spy(Pet.class);
-		pet1.setName("Lexi");
-		Pet pet2 = spy(Pet.class);
-		pet2.setName("Luxi");
-		Pet pet3 = spy(Pet.class);
-		pet3.setName("Luxe");
-		Pet pet4 = spy(Pet.class);
-		pet4.setName("Coco");
-		owner.addPet(pet1);
-		owner.addPet(pet2);
-		owner.addPet(pet3);
-		owner.addPet(pet4);
-		List<Pet> expected = Arrays.asList(pet4, pet1, pet3, pet2);
+		List<Pet> expected = createAndAddMultiplePets();
 		List<Pet> result = owner.getPets();
-		assertEquals(expected, result);
+		assertEquals(expected, result, "list is not properly sorted");
 	}
 
+	@Test
+	public void testGetPetCaseInsensitive() {
+		callAddPet();
+		String petMockUpperCaseName = petMock.getName().toUpperCase();
+		Pet foundPet = owner.getPet(petMockUpperCaseName, false);
+		assertEquals(foundPet, petMock, "getPet is case sensitive");
+	}
 
 	@DataPoints
 	public static boolean[] ignoreNews = {true, false};
 
-	@DataPoint
-	public static String name = "pishul";
+	@DataPoints
+	public static String[] names  = {"pishul","coco","lexi"};
 
-	@Theory
-	public void testNoPetReturnIfNotExist(boolean ignoreNew, String name) {
-		Set<String> petNames = new HashSet<>();
-		List<Pet> pets = owner.getPets();
-		for (Pet pet :  pets)
-			petNames.add(pet.getName());
-		assumeFalse(petNames.contains(name));
-		Pet retPet = owner.getPet(name, ignoreNew);
-		assertTrue(retPet == null);
+	@DataPoints
+	public static List<List<String>> petNames (){
+		List<List<String>> petNames = new ArrayList<>();
+		petNames.add(Arrays.asList("dolme","kadoo"));
+		petNames.add(Arrays.asList("pishul","pishi","pish"));
+
+		return petNames;
 	}
 
 	@Theory
-	public void testFindPetIfNotNew(boolean ignoreNew, String name) {
+	public void testGetPetNoPetReturnIfNotExist(List<String> petNames, String name) {
+		assumeTrue(!petNames.contains(name));
+		addPets(petNames);
+		Pet retPet = owner.getPet(name,false);
+		assertNull(retPet, "pet should not be found");
+	}
+
+	@Theory
+	public void testGetPetIfNotNew(List<String> petNames, String name, boolean ignoreNew) {
 		callAddPet();
-		Set<String> petNames = new HashSet<>();
-		List<Pet> pets = owner.getPets();
-		for (Pet pet :  pets)
-			petNames.add(pet.getName().toLowerCase());
 		assumeTrue(petNames.contains(name.toLowerCase()));
+		assumeTrue(name.equals(petMock.getName()));
 		when(petMock.isNew()).thenReturn(false);
 		Pet retPet = owner.getPet(name, ignoreNew);
-		assertEquals(retPet.getName().toLowerCase(), name.toLowerCase());
+		assertEquals(retPet.getName().toLowerCase(), name.toLowerCase(), "found pet is not what expected");
 	}
 
 }
--- src/test/java/org/springframework/samples/petclinic/owner/PetTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetTest.java
@@ -1,11 +1,67 @@
 package org.springframework.samples.petclinic.owner;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Assume;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 import org.springframework.samples.petclinic.visit.Visit;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-class PetTest {
+@RunWith(Theories.class)
+public class PetTest {
 
-	// todo
+	private static Visit visit1,visit2,visit3;
+	Pet pet;
+
+	@Before
+	public  void setup() {
+		pet = new Pet();
+	}
+
+
+	@DataPoints
+	public static List<List<Visit>> visits() {
+		visit1 = new Visit();
+		visit2 = new Visit();
+		visit3 = new Visit();
+
+		visit1.setDate(LocalDate.of(2021,6,17));
+		visit2.setDate(LocalDate.of(2021,1,1));
+		visit3.setDate(LocalDate.of(2021,10,15));
+
+		List<List<Visit>> visits = new ArrayList<>();
+		visits.add(Arrays.asList(visit1,visit2,visit3));
+		visits.add(Arrays.asList(visit2,visit3,visit1));
+		visits.add(Arrays.asList(visit3,visit2,visit1));
+		visits.add(Arrays.asList(visit3,visit1,visit2));
+		visits.add(Arrays.asList(visit2,visit1,visit3));
+		visits.add(Arrays.asList(visit1,visit3,visit2));
+		return visits;
+
+	}
+
+
+	@Theory
+	public void testGetVisitSortedDescending(List<Visit> visits){
+
+		Assume.assumeNotNull(visits);
+
+		List<LocalDate> sortedList = Arrays.asList(visit3.getDate(),visit1.getDate(),visit2.getDate());
+		visits.forEach(pet::addVisit);
+		List<LocalDate> actualDates = new ArrayList<>();
+		for(Visit visit : pet.getVisits())
+			actualDates.add(visit.getDate());
+
+		Assert.assertEquals(sortedList,actualDates);
+
+	}
+
+
 }
